06fab34bc40141a1ab080903978ed902a4585ae0
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2024-04-04 11:04:40 -0500

    updated

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+
+export default {
+  entry: "./app/index.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 3000,
+    hot: true
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "../app/views/index.ejs" }), new HtmlWebpackPlugin({ template: "../app/views/about.ejs" }), new HtmlWebpackPlugin({ template: "../app/views/contact.ejs" }), new HtmlWebpackPlugin({ template: ".../app/views/partials/footer.ejs" }), new HtmlWebpackPlugin({ template: ".../app/views/partials/header.ejs" })],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: "style-loader"
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: "css-loader"
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
